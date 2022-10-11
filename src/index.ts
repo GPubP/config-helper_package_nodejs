104b6034ce4c27400f1928e44e01263416356717
smtsj <jo.smets@studiohyperdrive.be>
2022-10-11 12:59:09 +0200

    fix: pass moduleid from value

--- src/index.ts
+++ src/index.ts
@@ -211,7 +211,7 @@ export class TenantsConfig extends EventEmitter {
 		this.modulesKafkaConsumer.on([
 			'module-updated',
 			'module-removed',
-		], data => this.onTick(propOr('config-updated', 'key', data), data as any));
+		], data => this.onTick(propOr('config-updated', 'key', data), { moduleId: (data as { value: { uuid: string } })?.value?.uuid }));
 	}
 
 	private onTick(key: string = 'config-updated', data?: Record<string, unknown>): void {
