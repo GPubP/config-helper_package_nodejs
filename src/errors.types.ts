050a733bf6ea5a20efa9f45cf9cd0ffd2c652a8b
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2023-09-25 15:38:06 +0200

    feat(errors): Add shared error filters for consistancy

---
+++ src/errors.types.ts
@@ -0,0 +1,36 @@
+import type { CustomValidationError } from './errors';
+
+export type ICustomValidationError = CustomValidationError;
+
+export interface ICustomErrorDetail {
+	err: string;
+}
+
+export interface ICustomError {
+	type: string;
+	title: string;
+	status: number;
+	identifier: string;
+	code: string;
+	extraInfo: unknown;
+}
+export type IBodyError = ICustomError;
+export type IHeadersError = ICustomError;
+export type IParamsError = ICustomError;
+export type IQueryError = ICustomError;
+export type IUnauthorizedError = ICustomError;
+export type IForbiddenError = ICustomError;
+export type INotFoundError = ICustomError;
+export type IConflictError = ICustomError;
+export type IInternalServerError = ICustomError;
+export type IErrors =
+	ICustomError |
+	IBodyError |
+	IHeadersError |
+	IParamsError |
+	IQueryError |
+	IUnauthorizedError |
+	IForbiddenError |
+	INotFoundError |
+	IConflictError |
+	IInternalServerError;
